b60bea46537d7066d69ffba281ac8e94cba6eefc
Fandy Barestu <fandy@softwareseni.com>
2021-01-14 15:33:00 +0700

    Create Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,63 @@
+import { ButtonHTMLAttributes, ReactElement, ReactNode } from 'react';
+import {
+  Container,
+  Column,
+  Row,
+  Link,
+  Title,
+  Text,
+  Break,
+} from './styles/Footer';
+
+type ComponentProps = {
+  children: ReactNode;
+};
+
+type LinkProps = {
+  children: ReactNode;
+  href: string;
+};
+
+function Footer({ children, ...restProps }: ComponentProps) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+Footer.Column = function FooterColumn({
+  children,
+  ...restProps
+}: ComponentProps) {
+  return <Column {...restProps}>{children}</Column>;
+};
+
+Footer.Row = function FooterRow({ children, ...restProps }: ComponentProps) {
+  return <Row {...restProps}>{children}</Row>;
+};
+
+Footer.Link = function FooterLink({
+  children,
+  href,
+  ...restProps
+}: LinkProps): ReactElement<LinkProps> {
+  return (
+    <Link href={href} {...restProps}>
+      {children}
+    </Link>
+  );
+};
+
+Footer.Title = function FooterTitle({
+  children,
+  ...restProps
+}: ComponentProps) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Footer.Text = function FooterText({ children, ...restProps }: ComponentProps) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Footer.Break = function FooterBreak({ ...restProps }) {
+  return <Break {...restProps} />;
+};
+
+export default Footer;
